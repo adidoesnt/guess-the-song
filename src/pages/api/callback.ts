3b748fc94b3e95b9b8c5aeabd00ad86845cd28f6
Aditya Banerjee <aditya_banerjee@icloud.com>
2023-04-11 13:29:26 +0800

    add spotify authorisation

---
+++ src/pages/api/callback.ts
@@ -0,0 +1,8 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default async function handler(
+  _: NextApiRequest,
+  res: NextApiResponse
+) {
+  res.status(200).json({ status: "OK" });
+}
---
+++ src/pages/api/index.ts
@@ -0,0 +1,22 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import SpotifyWebApi from 'spotify-web-api-node';
+
+const redirectUri = process.env.ENV == "DEV" ? process.env.REDIRECT_URI_DEV : process.env.REDIRECT_URI_PROD;
+
+const spotifyApi = new SpotifyWebApi({
+  clientId: process.env.CLIENT_ID,
+  clientSecret: process.env.CLIENT_SECRET,
+  redirectUri
+});
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method === 'GET') {
+    const scopes = ['user-library-read'];
+    const state = "authorising";
+    const authorizeUrl = spotifyApi.createAuthorizeURL(scopes, state);
+    res.redirect(authorizeUrl);
+  }
+}
